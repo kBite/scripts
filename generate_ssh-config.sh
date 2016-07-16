e6afc047387134ab52c818c41290d0ffc1009894
Kilian <kilian.engelhardt@gmail.com>
2016-07-16 18:30:56 +0200

    symlink ...

--- generate_ssh-config.sh
+++ generate_ssh-config.sh
@@ -1,1 +1,17 @@
-/home/kayle/git-repositories/scripts.git/bash/generate_ssh-config.sh
+#!/bin/bash
+
+# kBite: generate_ssh-config.sh			early alpha, production ready
+#
+# OpenSSH 7.3 will have an include feature, but every version below does not.
+# Reference: https://bugzilla.mindrot.org/show_bug.cgi?id=1585
+#
+# This is my workaround.
+
+if [[ ! -d ${HOME}/.ssh/config.d ]]; then
+	mkdir ${HOME}/.ssh/config.d
+	echo "Your first run?"
+	exit 123
+fi
+
+cat ${HOME}/.ssh/config.d/*.cfg > ${HOME}/.ssh/config
+exit 0
